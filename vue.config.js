3e234e14d55d4b677b1aae826df662b3df80c850
Andrew Asplund <andrew.j.asplund@gmail.com>
2020-04-08 14:49:03 -0700

    Updated config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
 module.exports = {
-    runtimeCompiler: true,
     css: {
         sourceMap: true
     },
     //build for docs folder to enable gh-pages hosting
-    outputDir: './docs/',
-    assetsDir: 'assets'
+    outputDir: './docs/'
 }
